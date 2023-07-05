3dd79c2d52716a5689c0b852ea78d7c208fca171
Bruna Platt <bruna.platt@hotmail.com>
2023-07-04 22:59:22 -0300

    Co-authored-by: Douglas Pereira <douglasspereira@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -36,51 +36,45 @@ function login() {
         }
     }
 
-
-	//let itens = [
-	//{id: "1", nome: "João", item: "Caneta", local: "Sala F21", imagem: "caneta.jpg"},
-	//{id: "2", nome: "Julia", item: "Celular", local: "Sala D22", imagem: "celular.jpg"},
-//]
-
-
-//function constroiItem(idItem, nome, nomeItem, localItem, imagemItem) {
-	//this.id = idItem;
-	//this.nome = nome;
-	//this.item = nomeItem;
-	//this.local = localItem;
-	//this.imagem = imagemItem;
-//}
-
-//function cadastrarItem(){	
-
-	//let nome = document.getElementById("nome").value; 
-	//let nomeItem = document.getElementById("nomeItem").value;
-	//let localItem = document.getElementById("localItem").value;
-	//let imagemItem = document.getElementById("imagemItem").value;
-	//localStorage.setItem("itens", JSON.stringify(itens));
-	//let itens2 = JSON.parse(localStorage.getItem("itens"));
-	//let idItem = parseFloat(itens2[itens2.length - 1].id) + 1
-
-	//if (nome == "" || nomeItem == "" || localItem == "" || imagemItem == "") {
-		//alert("Preencha todos os campos")
-		//var globo = 1;
-	//} else if(globo !==1) {
-		//var add = new constroiItem(String(idItem), nome, nomeItem, localItem, imagemItem)
-
-		//if(!localStorage.itens){
-			//itens = []
-		//} else {
-			//itens = JSON.parse(localStorage.itens)
-		//}
-
-		//itens.push(add)
-		//localStorage.setItem("itens", JSON.stringify(itens));
-		//console.log(Object.values(itens))
-		//alert("Item cadastrado com sucesso")
-
-		//document.getElementById("nome").value = null;
-		//document.getElementById("nomeItem").value = null;
-		//document.getElementById("localItem").value = null;
-		//document.getElementById("imagemItem").value = null;
-	//}
-//}
+	function constroiItem(idItem, nome, nomeItem, localItem, imagemItem) {
+		this.id = idItem;
+		this.nome = nome;
+		this.item = nomeItem;
+		this.local = localItem;
+		this.imagem = imagemItem;
+	}
+	
+	function cadastrarItem(){	
+	
+		let nome = document.getElementById("nome").value; 
+		let nomeItem = document.getElementById("nomeItem").value;
+		let localItem = document.getElementById("localItem").value;
+		let imagemItem = document.getElementById("imagemItem").value;
+		localStorage.setItem("itens", JSON.stringify(itens));
+		let itens2 = JSON.parse(localStorage.getItem("itens"));
+		let idItem = parseFloat(itens2[itens2.length - 1].id) + 1
+	
+		if (nome == "" || nomeItem == "" || localItem == "" || imagemItem == "") {
+			alert("Preencha todos os campos")
+			var globo = 1;
+		} else if(globo !==1) {
+			var add = new constroiItem(String(idItem), nome, nomeItem, localItem, imagemItem)
+	
+			if(!localStorage.itens){
+				itens = []
+			} else {
+				itens = JSON.parse(localStorage.itens)
+			}
+	
+			itens.push(add)
+			localStorage.setItem("itens", JSON.stringify(itens));
+			console.log(Object.values(itens))
+			alert("Item cadastrado com sucesso")
+	
+			document.getElementById("nome").value = null;
+			document.getElementById("nomeItem").value = null;
+			document.getElementById("localItem").value = null;
+			document.getElementById("imagemItem").value = null;
+		}
+	}
+	
