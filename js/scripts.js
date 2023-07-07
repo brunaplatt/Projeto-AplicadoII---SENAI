fc1fd509778a6c729cad8a37483d2730a40f9b85
Bruna Platt <bruna.platt@hotmail.com>
2023-07-07 19:49:40 -0300

    07.07.23

--- js/scripts.js
+++ js/scripts.js
@@ -41,6 +41,123 @@ function login() {
         }
     }
 
+	function Enviar()
+{
+alert("Enviado com Sucesso!");
+}
+
+	$(function(){
+		var operacao = "A"; //"A"=Adição; "E"=Edição
+		var indice_selecionado = -1; //Índice do item selecionado na lista
+		var tbItens = localStorage.getItem("tbItens");// Recupera os dados armazenados
+		tbItens = JSON.parse(tbItens); // Converte string para objeto
+		if(tbItens == null) // Caso não haja conteúdo, iniciamos um vetor vazio
+		tbItens = [];
+	});
+
+	function Adicionar(){
+		var itens = JSON.stringify({
+			Nome       : $("#Nome").val(),
+			Descricao  : $("#Descricao").val(),
+			Local      : $("#Local").val(),
+			Data       : $("#Data").val(),
+			Imagem     : $("#Imagem").file()
+	});
+		tbItens.push(itens);
+		localStorage.setItem("tbItens", JSON.stringify(tbItens));
+		alert("Registro adicionado.");
+		return true;
+	}
+
+	function Editar(){
+		tbItens[indice_selecionado] = JSON.stringify({
+			Nome       : $("#Nome").val(),
+			Descricao  : $("#Descricao").val(),
+			Local      : $("#Local").val(),
+			Data       : $("#Data").val(),
+			Imagem     : $("#Imagem").file()
+			});//Altera o item selecionado na tabela
+		localStorage.setItem("tbItens", JSON.stringify(tbItens));
+		alert("Informações editadas.")
+		operacao = "A"; //Volta ao padrão
+		return true;
+	}
+
+	function Excluir(){
+		tbItens.splice(indice_selecionado, 1);
+		localStorage.setItem("tbItens", JSON.stringify(tbItens));
+		alert("Registro excluído.");
+	}
+
+	function Listar(){
+		$("#tblLiWistar tbody").append("</tr>");
+	}
+
+	
+	function Listar(){
+    	$("#tblListar").html("lista.html");
+    	$("#tblListar").html(
+			"<thead>"+
+			"	<tr>"+
+			"<th></th>"+
+			"	<th>Nome</th>"+
+			"	<th>Descrição</th>"+
+			"	<th>Local</th>"+
+			"	<th>Data</th>"+
+			"	</tr>"+
+			"</thead>"+
+			"<tbody>"+
+			"</tbody>"
+		);
+
+
+    	for(var i in tbItens){
+       		var it = JSON.parse(tbItens[i]);
+        	$("#tblListar tbody").append("<tr>");
+        	$("#tblListar tbody").append("<td><img src='img/edit.png' alt='"+i+"' class = 'btnEditar'/><img src='img/delete.png' alt='"+i+"' class='btnExcluir'/></td>");
+       		$("#tblListar tbody").append("<td>"+it.Nome+"</td>");
+        	$("#tblListar tbody").append("<td>"+it.Descricao+"</td>");
+        	$("#tblListar tbody").append("<td>"+it.Local+"</td>");
+			$("#tblListar tbody").append("<td>"+it.Data+"</td>");
+        	$("#tblListar tbody").append("</tr>");
+   		}
+	}
+
+	$("#Cadastro").on("submit",function(){
+    	if(operacao == "A")
+        	return Adicionar();
+    	else
+        	return Editar();
+	});
+
+
+	$("#tblListar").on("click", ".btnEditar", function(){
+    	operacao = "E";
+    	indice_selecionado = parseInt($(this).attr("alt"));
+    	var it = JSON.parse(tbItens[indice_selecionado]);
+    	$("#Nome").val(it.Nome);
+    	$("#Descricao").val(it.Descricao);
+    	$("#Local").val(it.Local);
+		$("#Data").val(it.Data);
+		//$("#txtCodigo").attr("readonly","readonly");
+    	$("#Nome").focus();
+	});
+
+	$("#tblListar").on("click", ".btnExcluir",function(){
+    	indice_selecionado = parseInt($(this).attr("alt"));
+    	Excluir();
+    	Listar();
+	});
+
+	$("#tblListar").on("click", ".btnExcluir", function(){
+		indice_selecionado = parseInt($(this).attr("alt"));
+		Excluir();
+		Listar();
+	});
+
+	/*  <button id="btnCadastrar" onclick="cadastrarItem()">Cadastrar</button> HTML
+        <button id="btnLogout" onclick="logout()">Cancelar</button> HTML
+	
 	function constroiItem(idItem, nome, nomeItem, localItem, imagemItem) {
 		this.id = idItem;
 		this.nome = nome;
@@ -48,7 +165,7 @@ function login() {
 		this.local = localItem;
 		this.imagem = imagemItem;
 	}
-	
+		
 	function cadastrarItem(){	
 	
 		let nome = document.getElementById("nome").value; 
@@ -61,8 +178,8 @@ function login() {
 	
 		if (nome == "" || nomeItem == "" || localItem == "" || imagemItem == "") {
 			alert("Preencha todos os campos")
-			var globo = 1;
-		} else if(globo !==1) {
+			var flag = 1;
+		} else if(flag !==1) {
 			var add = new constroiItem(String(idItem), nome, nomeItem, localItem, imagemItem)
 	
 			if(!localStorage.itens){
@@ -83,3 +200,49 @@ function login() {
 		}
 	}
 	
+	function imprimiItens(){
+	
+		function criaTag(elemento) {
+			return document.createElement(elemento)
+		}
+	
+		let tabela = document.getElementById("tbitens");
+		var linha = criaTag("tr");
+		// let coluna = criaTag("td");        
+	
+		let itensTabela = JSON.parse(localStorage.getItem("itens"));
+	
+		itensTabela.sort(ordenacao);
+	
+		function criaCelula (tag, text){
+			tag = criaTag(tag);
+			tag.textContent = text;
+			return tag;
+		}
+	
+		let id = "";
+		let nome = "";
+		let item = "";
+		let local = "";    
+		let imagem = "";    
+	
+		for(let j = 0; j < itensTabela.length; j++){
+	
+			var linha = criaTag("tr");
+			tabela.appendChild(linha);
+			id = criaCelula("td", itensTabela[j].id);
+			linha.appendChild(id);
+			nome = criaCelula("td", itensTabela[j].nome);
+			linha.appendChild(nome);
+			item = criaCelula("td", itensTabela[j].item);
+			linha.appendChild(item);
+			local = criaCelula("td", itensTabela[j].local);
+			linha.appendChild(local);
+			imagem = criaCelula("td", itensTabela[j].imagem);
+			linha.appendChild(imagem);
+		}
+	}
+	
+	function ordenacao(a, b, c, d, e){
+		return a.id - b.id;
+	}*/
